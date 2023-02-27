73434cfcf6f9a541e937f3f8c54f766f0a51b140
DAN <58280710+daniel081009@users.noreply.github.com>
2023-02-27 14:49:54 +0900

    clear && create README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO
+
+## 간단한 CLI TODO 프로그램입니다
+
+```console
+go build -o td main.go
+```
+
+그리고 윈도우라면 환경설정 등록을 해주고 mac이라면 .zshrc 같은 파일에서 alias에 등록하면 편하게 사용할 수 있어요!
--- cmd/root.go
+++ cmd/root.go
@@ -2,15 +2,10 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"todo/db"
 
 	"github.com/spf13/cobra"
-
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "td",
@@ -27,32 +22,4 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".go-gopher-cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".go-gopher-cli")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
